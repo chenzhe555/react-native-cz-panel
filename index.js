10c6810bec81555d7d25aed68ed5c053b63d6607
chenzhe <chenzhe@meicai.cn>
2019-01-23 23:42:29 +0800

    init project

---
+++ index.js
@@ -0,0 +1,6 @@
+
+import { NativeModules } from 'react-native';
+
+const { RNCzPanel } = NativeModules;
+
+export default RNCzPanel;
---
+++ ios/RNCzPanel.m
@@ -0,0 +1,13 @@
+
+#import "RNCzPanel.h"
+
+@implementation RNCzPanel
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+RCT_EXPORT_MODULE()
+
+@end
+  
